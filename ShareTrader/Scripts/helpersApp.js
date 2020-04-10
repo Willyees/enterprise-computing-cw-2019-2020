47e98cbd7782909909bdc764dbd0c21b0ff79e0c
Alessio Williams Gava <marveel@hotmail.it>
2020-04-10 10:37:12 +0100

    moved common used functions into the helpersapp js script. Using login and register as main page

---
+++ ShareTrader/Scripts/helpersApp.js
@@ -0,0 +1,6 @@
+﻿function isLoggedIn() {
+    if (!sessionStorage.getItem('accessToken')) {
+        alert("not logged in, redirecting to register page")
+        window.location.href = "/";
+    }
+}
